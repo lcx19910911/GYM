fd9a67e34a20e83f3ec534188b01c802c6aa2185
Administrator <Administrator@PC-20170504PCLA>
2017-09-22 15:36:50 +0800

    权限过滤

--- GYM.Web.Framework/Filters/AdminLoginFilterAttribute.cs
+++ GYM.Web.Framework/Filters/AdminLoginFilterAttribute.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using GYM.IService;
+using GYM.Core.Model;
+using GYM.Core.Code;
+using GYM.Service;
 
 namespace GYM.Web.Framework.Filters
 {
@@ -12,6 +16,11 @@ namespace GYM.Web.Framework.Filters
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
     public class AdminLoginFilterAttribute : ActionFilterAttribute
     {
+        public AdminLoginFilterAttribute()
+        { }
+        IMenuService IMenuService=new MenuService();
+        IOperateService IOperateService = new OperateService();
+
         public override void OnActionExecuting(ActionExecutingContext filterContext)
         {
             var controller = filterContext.Controller as BaseAdminController;
@@ -26,11 +35,37 @@ namespace GYM.Web.Framework.Filters
                 RedirectResult redirectResult = new RedirectResult("/login/index?redirecturl=" + requestUrl);
                 filterContext.Result = redirectResult;
             }
-            if (controller.LoginAdmin == null)
+
+            if (controller.LoginAdmin != null)
             {
-                if (!controllerName.Equals("login", StringComparison.OrdinalIgnoreCase))
+                var url = filterContext.HttpContext.Request.RawUrl;
+                if (!controller.LoginAdmin.IsSuperAdmin)
                 {
-                    RedirectResult redirectResult = new RedirectResult("/admin/login/index?redirecturl=" + requestUrl);
+                    if (filterContext.HttpContext.Request.HttpMethod == "Get")
+                    {
+                        if (!IMenuService.IsHavePage(url))
+                        {
+                            filterContext.Result = new RedirectResult("/Admin/Home/Index");
+                        }
+                    }
+                    else
+                    {
+                        if (!IOperateService.IsHaveAuthority(url))
+                        {
+                            var result = new WebResult<bool> { Code = ErrorCode.sys_user_role_error, Result = false };
+                            JsonResult jsonResult = new JsonResult();
+                            jsonResult.Data = result;
+                            jsonResult.JsonRequestBehavior = JsonRequestBehavior.AllowGet;
+                            filterContext.Result = jsonResult;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                if (!controllerName.Equals("Login", StringComparison.CurrentCultureIgnoreCase))
+                {
+                    RedirectResult redirectResult = new RedirectResult("/Admin/login/index?redirecturl=" + requestUrl);
                     filterContext.Result = redirectResult;
                 }
             }
